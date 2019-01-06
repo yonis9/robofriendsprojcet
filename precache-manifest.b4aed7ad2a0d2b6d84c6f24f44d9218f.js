9e4059c0e5c1ef802510464d2bc48bdb49c9ee4a
Yoni Sisso <yonis9@walla.com>
2019-01-06 04:46:58 +0200

    Updates

---
+++ precache-manifest.b4aed7ad2a0d2b6d84c6f24f44d9218f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriendsprojcet/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "a747ae19593b175234ec",
+    "url": "/robofriendsprojcet/static/js/runtime~main.a747ae19.js"
+  },
+  {
+    "revision": "9f9b41dfd802b7f15587",
+    "url": "/robofriendsprojcet/static/js/main.9f9b41df.chunk.js"
+  },
+  {
+    "revision": "165857fead4b90a650e9",
+    "url": "/robofriendsprojcet/static/js/1.165857fe.chunk.js"
+  },
+  {
+    "revision": "9f9b41dfd802b7f15587",
+    "url": "/robofriendsprojcet/static/css/main.e8340ee5.chunk.css"
+  },
+  {
+    "revision": "165857fead4b90a650e9",
+    "url": "/robofriendsprojcet/static/css/1.0560fff6.chunk.css"
+  },
+  {
+    "revision": "60a30f6cb2476d342799f8c17e231728",
+    "url": "/robofriendsprojcet/index.html"
+  }
+];
